0ab221a51002cdad953ec3fe01df263bcd2db953
ssunnyku <ssunnykku@naver.com>
2022-07-04 10:34:46 +0900

    메세지

---
+++ practice.js
@@ -0,0 +1,17 @@
+// for (var i = 0; i < 10; i ++) {
+//     console.log(i);
+// }
+
+// var num = 0;
+
+// while (num < 10) {
+//     console.log(num);
+//     num++
+// }
+
+var i = 12;
+
+do {
+    console.log(i);
+    i++;
+} while (i < 10);
